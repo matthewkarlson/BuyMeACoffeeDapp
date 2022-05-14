b1d9683defe13b771c2f685c9214a10a180ddfba
Ciffsome <mattjkarlson@gmail.com>
2022-05-14 17:37:02 +0100

    Fixed error

--- BuyMeACoffee-contracts/contracts/BuyMeACoffee.sol
+++ BuyMeACoffee-contracts/contracts/BuyMeACoffee.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 //deployed to 0xB16F3a66D064969b3c8509A2929d1bDC583CD842
-import "hardhat/console.sol";
 
 contract BuyMeACoffee {
     event NewMemo(
